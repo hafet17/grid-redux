3e12146a27026ac239a2cf663da176d0dcaa442c
Hafet17 <do.crazy192@gmail.com>
2021-01-18 15:45:35 +0700

    create Tile

---
+++ src/components/Tile/index.js
@@ -0,0 +1,18 @@
+import * as React from 'react';
+import './style.css';
+import { string, number } from 'prop-types'
+
+export default function Tile ({ color, id }) {
+    return (
+        <div className="tile" style={{ backgroundColor: color }} />
+    )
+}
+
+Tile.defaultProps = {
+    color: 'gray'
+}
+
+Tile.propTypes = {
+    color: string,
+    id: number.isRequired
+}
