ee8636de0a8d68899378161777cdbba728cfabd9
Hafet17 <do.crazy192@gmail.com>
2021-01-28 10:22:48 +0700

    add Grid components in App

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import { Provider } from 'react-redux'
 import Store from './app/store'
+import Grid from './components/Grid'
 
 function App () {
   return (
     <div>
-      <Provider store={Store} />
+      <Provider store={Store} >
+        <Grid />
+      </Provider>
     </div>
   );
 }
