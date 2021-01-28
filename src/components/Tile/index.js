6dbf4ddfce60e000967feaf647c3465113074510
Hafet17 <do.crazy192@gmail.com>
2021-01-28 10:22:25 +0700

    add DoubleClick in Tile

--- src/components/Tile/index.js
+++ src/components/Tile/index.js
@@ -2,9 +2,9 @@ import * as React from 'react';
 import './style.css';
 import { string, number } from 'prop-types'
 
-export default function Tile ({ color, id }) {
+export default function Tile ({ color, id, onDoubleClick }) {
     return (
-        <div className="tile" style={{ backgroundColor: color }} />
+        <div className="tile" style={{ backgroundColor: color }} onDoubleClick={onDoubleClick} />
     )
 }
 
